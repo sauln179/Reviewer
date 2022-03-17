cd141e837da8d7867da3b777a7fb515c66c8c870
sauln179 <79173328+sauln179@users.noreply.github.com>
2022-03-17 15:28:32 -0700

    Update tmd.js

--- tmd.js
+++ tmd.js
@@ -1,12 +1,15 @@
 var searchEl = document.getElementById("search-input");
-var buttonEle = document.getElementById("m-button");
+var movieButton = document.getElementById("m-button");
+var tvButton = document.getElementById("t-button")
 var outputEl = document.getElementById("output");
 
-var mediaSearch = function(){
+var movieSearch = function(){
     var movie = searchEl.value;
+     //Replaces space with % for query
     var input = movie.replaceAll(" ", '%');
     
     var requestUrl = 'https://api.themoviedb.org/3/search/movie?api_key=039516e5ffedf8edd44caa8482c60cda&language=en-US&query='+input+'&page=1&include_adult=false';
+   
     console.log(requestUrl);
 
 
@@ -21,40 +24,93 @@ fetch(requestUrl)
         }
 
         console.log(data);
-
-        var date = [];
-        var title = [];
         
-        
-        for (i = 0; i < 9; i++) {
-            if (typeof data.docs[i].release_date !== 'undefined') {
-                date.push(data.docs[i].release_date);
-            }
-            else {
-                date.push('Unknown');
-            }
-
-            if (typeof data.docs[i].original_title !== 'undefined') {
-                title.push(data.docs[i].title + ':' + data.docs[i].original_title);
-            }
-            else {
-                title.push(data.docs[i].title);
-            }
-
-            // entire div box, containing the book and elements within
+        //Results retireves up to 6 items that match up.
+        for (i = 0; i < 6; i++) {
             var mediaBox = document.createElement('div');
             mediaBox.className = 'container px-6 py-20 bg-gray-100 m-2 shadow w-full min-w-xl';
-
-            // Book Title and the Author's name
+            //Obtains date released, and title.
             var header = document.createElement('h2');
             header.className = 'text-2xl font-bold text-center text-gray-800 mb-8'
-            header.textContent = title[i] + ' released on' + date[i];
+            header.textContent = data.results[i].title + ':'+'released on ' + data.results[i].release_date;
 
+            //Obtains summary.
+            var summary = document.createElement('p');
+            summary.className = "text-2xl font-bold text-center text-gray-800 mb-8"
+            summary.textContent = data.results[i].overview
+
+            var cover = document.createElement('img');
+            cover.className = 'mx-auto rounded-xl text-center';
+            //Gets poster size
+            var image = 'https://image.tmdb.org/t/p/w500/' +data.results[i].poster_path;
+            cover.src =  image;
+            cover.alt = data.results[i].title;
+            mediaBox.appendChild(cover);
             mediaBox.appendChild(header);
-            outputEl.appendChild(mediaBox);
-
+            mediaBox.appendChild(summary);
+            outputEl.appendChild(mediaBox); 
+          
+           
         }
 
         })
 }
-buttonEle.addEventListener("click", mediaSearch);
+var tvSearch = function(){
+    var movie = searchEl.value;
+    //Replaces space with % for query
+    var input = movie.replaceAll(" ", '%');
+    
+    var requestUrl = 'https://api.themoviedb.org/3/search/tv?api_key=039516e5ffedf8edd44caa8482c60cda&language=en-US&page=1&query='+input+'&include_adult=false';
+   
+    console.log(requestUrl);
+
+
+fetch(requestUrl)
+        .then(function(response) {
+        return response.json();
+    })
+        .then(function(data) {
+        // Checks if something is already in the output zone
+        while (outputEl.firstChild) {
+            outputEl.removeChild(outputEl.firstChild);
+        }
+
+        console.log(data);
+        
+        //When searched for, it will produce, 6 items.
+        //Creates its own box for each item.
+        //Same as above, but for TV shows instead.
+        for (i = 0; i < 6; i++) {
+            var mediaBox = document.createElement('div');
+            mediaBox.className = 'container px-6 py-20 bg-gray-100 m-2 shadow w-full min-w-xl';
+        
+            var header = document.createElement('h2');
+            header.className = 'text-2xl font-bold text-center text-gray-800 mb-8'
+            header.textContent = data.results[i].name + ':'+' first aired on ' + data.results[i].first_air_date;
+
+
+            var summary = document.createElement('p');
+            summary.className = "text-2xl font-bold text-center text-gray-800 mb-8"
+            summary.textContent = data.results[i].overview
+
+            var cover = document.createElement('img');
+            cover.className = 'mx-auto rounded-xl text-center';
+            var image = 'https://image.tmdb.org/t/p/w500/' +data.results[i].poster_path;
+            cover.src =  image;
+            cover.alt = data.results[i].name;
+            mediaBox.appendChild(cover);
+            mediaBox.appendChild(header);
+            mediaBox.appendChild(summary);
+            outputEl.appendChild(mediaBox); 
+          
+        
+        }
+
+        })
+}
+
+
+
+
+movieButton.addEventListener("click", movieSearch);
+tvButton.addEventListener("click", tvSearch);
